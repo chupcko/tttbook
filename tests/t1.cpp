d175063730053efa2cb93da39afb70a6e071f726
Goran Lazic <github@chupcko.org>
2013-03-28 22:42:02 +0100

    Dodat test_util::triplet zarad testiranja

--- tests/t1.cpp
+++ tests/t1.cpp
@@ -4,43 +4,34 @@
 #include <utility>
 
 #include "tttbook.hpp"
+#include "triple.hpp"
 
 using namespace std;
 using namespace tttbook;
+using namespace test_util;
 
-template <class C1, class C2, class C3> struct triplet
-{
-  C1 element_1;
-  C2 element_2;
-  C3 element_3;
-};
-
+typedef vector<pair<int, int>> moves_t;
+typedef triplet<int, array<bool, 3>, moves_t> test_t;
 
 int main()
 {
-  triplet<bool, int, char> pera = { true, 2, '3'};
-
-  vector
-  <
-    pair
-    <
-      array<int, 3>,
-      vector<pair<int, int>>
-    >
-  > result =
+  vector<test_t> tests =
   {
-    { {{ 0, 1, 0 }},  { {0, 0}, {0, 2}, {1, 0}, {1, 2}, {2, 0} } },
+    { 1, {{ false, true, false }},  { {0, 0}, {0, 2}, {1, 0}, {1, 2}, {2, 0} } },
   };
   table m;
 
 
 
-  for(auto& el: result)
+  for(auto& test: tests)
   {
     m.init();
-    for(auto& igra: el.second)
-      m.play(igra.first, igra.second);
-    cout << "Ocekivano " << el.first[0] << ' ' << el.first[1] << ' ' << el.first[2] << '\n';
+    auto& number = get<0>(test);
+    auto& except = get<1>(test);
+    auto& moves = get<2>(test);
+    for(auto& move: moves)
+      m.play(move.first, move.second);
+    cout << "Ocekivano " << except[0] << ' ' << except[1] << ' ' << except[2] << '\n';
     cout << "Dobio " << m.is_draw() << ' ' << m.is_win(m.GAMER_X) << ' ' << m.is_win(m.GAMER_O) << '\n';
   }
 
---
+++ tests/triple.hpp
@@ -0,0 +1,123 @@
+#ifndef __TRIPLET_HPP__
+#define __TRIPLET_HPP__
+
+namespace test_util
+{
+
+  template <class C1, class C2, class C3>
+  struct triplet
+  {
+    typedef C1 type_1;
+    typedef C2 type_2;
+    typedef C3 type_3;
+
+    C1 element_1;
+    C2 element_2;
+    C3 element_3;
+  };
+
+  template <std::size_t N, class C>
+  struct tuple_element;
+
+  template <class C1, class C2, class C3>
+  struct tuple_element<0, triplet<C1, C2, C3>>
+  {
+    typedef C1 type;
+  };
+
+  template <class C1, class C2, class C3>
+  struct tuple_element<1, triplet<C1, C2, C3>>
+  {
+    typedef C2 type;
+  };
+
+  template <class C1, class C2, class C3>
+  struct tuple_element<2, triplet<C1, C2, C3>>
+  {
+    typedef C3 type;
+  };
+
+  template <std::size_t N>
+  struct __triple_get;
+
+  template<>
+  struct __triple_get<0>
+  {
+    template <typename C1, typename C2, typename C3>
+    static constexpr C1& __get(triplet<C1, C2, C3>& t) noexcept
+    {
+      return t.element_1;
+    }
+    template <typename C1, typename C2, typename C3>
+    static constexpr C1&& __move_get(triplet<C1, C2, C3>&& t) noexcept
+    {
+      return std::forward<C1>(t.element_1);
+    }
+    template <typename C1, typename C2, typename C3>
+    static constexpr const C1& __const_get(const triplet<C1, C2, C3>& t) noexcept
+    {
+      return t.element_1;
+    }
+  };
+
+  template<>
+  struct __triple_get<1>
+  {
+    template <typename C1, typename C2, typename C3>
+    static constexpr C2& __get(triplet<C1, C2, C3>& t) noexcept
+    {
+      return t.element_2;
+    }
+    template <typename C1, typename C2, typename C3>
+    static constexpr C2&& __move_get(triplet<C1, C2, C3>&& t) noexcept
+    {
+      return std::forward<C1>(t.element_2);
+    }
+    template <typename C1, typename C2, typename C3>
+    static constexpr const C2& __const_get(const triplet<C1, C2, C3>& t) noexcept
+    {
+      return t.element_2;
+    }
+  };
+
+  template<>
+  struct __triple_get<2>
+  {
+    template <typename C1, typename C2, typename C3>
+    static constexpr C3& __get(triplet<C1, C2, C3>& t) noexcept
+    {
+      return t.element_3;
+    }
+    template <typename C1, typename C2, typename C3>
+    static constexpr C3&& __move_get(triplet<C1, C2, C3>&& t) noexcept
+    {
+      return std::forward<C1>(t.element_2);
+    }
+    template <typename C1, typename C2, typename C3>
+    static constexpr const C3& __const_get(const triplet<C1, C2, C3>& t) noexcept
+    {
+      return t.element_3;
+    }
+  };
+
+  template <std::size_t N, class C1, class C2, class C3>
+  constexpr typename tuple_element<N, triplet<C1, C2, C3>>::type& get(triplet<C1, C2, C3>& t) noexcept
+  {
+    return __triple_get<N>::__get(t);
+  }
+
+  template <std::size_t N, class C1, class C2, class C3>
+  constexpr typename tuple_element<N, triplet<C1, C2, C3>>::type&& get(triplet<C1, C2, C3>&& t) noexcept
+  {
+    return __triple_get<N>::__move_get(std::move(t));
+  }
+
+  template <std::size_t N, class C1, class C2, class C3>
+  constexpr const typename tuple_element<N, triplet<C1, C2, C3>>::type& get(const triplet<C1, C2, C3>& t) noexcept
+  {
+    return __triple_get<N>::__const_get(t);
+  }
+
+}
+
+#endif
